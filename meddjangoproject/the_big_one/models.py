d436c303704f4b12d2b3da04b103ca91ba1ca6de
Howie Benefiel <hben592@gmail.com>
2015-11-29 20:28:21 -0600

    Started working on doctors and addresses models

--- meddjangoproject/the_big_one/models.py
+++ meddjangoproject/the_big_one/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+class Doctor(models.Model):
+  first_name = models.CharField(max_length = 100)
+  last_name = models.CharField(max_length = 100)
+
+class Address(models.model):
+    street_address = models.CharField(max_length = 250)
+    city = models.CharField(max_length = 150)
+    state = models.CharField(max_length = 2)
+    zip_code = models.CharField(max_length = 5)
+
+
 # Create your models here.
