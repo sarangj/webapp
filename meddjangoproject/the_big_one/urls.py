5302af132bd3359764229a6b034bdc602b6ae8ce
Howie Benefiel <hben592@gmail.com>
2015-12-27 16:42:28 -0600

    Fixed the issue where '.../doctors/' was throwing an error instead of taking the user to a list of doctors

--- meddjangoproject/the_big_one/urls.py
+++ meddjangoproject/the_big_one/urls.py
@@ -9,6 +9,6 @@ from . import views
 # TODO This breaks when you go to .../doctors/ instead of .../doctors. We
 # should probably fix this
 urlpatterns = [
-    url(r'^doctors$', views.index, name='index'),
+    url(r'^doctors($|\/$)', views.doc_index, name='index'),
     url(r'^doctors/(?P<doctor_id>[0-9]+)/$', views.doctor, name='doctor'),
     ]
