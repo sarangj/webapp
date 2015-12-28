b7ff6e9de8bfcbcc1f9bbd5da04dd332908dc52d
sarangj <sarangjoshi22@gmail.com>
2015-12-27 22:15:59 -0600

    Add entry in admin.py for DoctorVisit

--- meddjangoproject/the_big_one/admin.py
+++ meddjangoproject/the_big_one/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 
 from .models import Doctor
+from .models import DoctorVisit
 from .models import Specialty
 
+admin.site.registed(Address)
 admin.site.register(Doctor)
+admin.site.register(DoctorVisit)
 admin.site.register(Specialty)
 
 # Register your models here.
